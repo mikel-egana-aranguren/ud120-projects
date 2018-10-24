27fbcbfd070bec7fea242cdea0af729d406cd737
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-10-24 10:00:27 +0200

    accuracy 0.97

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,12 +21,27 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
+
+### use the trained (fit) classifier to make a prediction from test features, ie create labels
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+### compare pedicted labels to test labels to obtain accuracy
+accuracy = accuracy_score(pred,labels_test)
+
+print ("accuracy: ", accuracy)
 
 #########################################################
 
