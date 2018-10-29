2d8f820da347f967544002919999487ec3d21b04
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2018-10-29 13:33:02 +0100

    Enron data explore

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,20 +19,50 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+
+
+
+# names_file = open("../final_project/poi_names.txt","r")
+# names_file_lines = names_file.readlines()
+# names_file.close
+# proper_names = []
+
+# for item in iter(names_file_lines):  
+#     if(item.startswith("(")):
+#         proper_name = ((((item[4:]).replace(",","")).upper()).rstrip()).lstrip()
+#         proper_names.append(proper_name)
+
+# for item in iter(proper_names):
+#     print (item)
+#     print (item == "LOEHR CHRISTOPHER")
+
 # print (enron_data)
 # print(enron_data["METTS MARK"]["salary"])
 # print(len(enron_data["METTS MARK"]))
 
-poi_count = 0
+# total_poi_count = 0
+
+# poi_count = 0
+# for key in enron_data:
+#     print (key[:-2])
+#     for item in iter(proper_names):
+#         if(item == key):
+#             total_poi_count += 1
+
+#     poi_status = enron_data[key]["poi"]
+#     if(poi_status):
+#         poi_count += 1
+# print (poi_count)
+
+# print (total_poi_count)
+
+# print (enron_data["SKILLING JEFFREY K"]["total_payments"])
+# print (enron_data["LAY KENNETH L"]["total_payments"])
+# print (enron_data["FASTOW ANDREW S"]["total_payments"])
+
+known = 0
 for key in enron_data:
-    poi_status = enron_data[key]["poi"]
-    print (poi_status)
-    if(poi_status):
-        print ("POI!")
-        poi_count += 1
-    else:
-        print ("false")
+    if(enron_data[key]["email_address"]!="NaN"):
+        known +=1
 
-print (poi_count)
-
-
+print (known) 
